abc65514aaa1fc2540b3f5c1995f3e06de9ba6cd
JGUIRIM Chaima <91907531+jguirimm@users.noreply.github.com>
2024-01-07 13:23:44 +0100

    Update pacman.testUnitaire.js

--- pacman.testUnitaire.js
+++ pacman.testUnitaire.js
@@ -2,19 +2,19 @@ import Pacman from './Pacman';
 import { OBJECT_TYPE, DIRECTIONS } from './setup';
 
 describe('Pacman Tests', () => {
-    // Pac-Man creation test
+    // creation de test Pacman
     test('Pac-Man is correctly created with initial properties', () => {
-        const pacman = new Pacman(1, 0); // Adjust the initial properties as needed
+        const pacman = new Pacman(1, 0); 
 
         expect(pacman.pos).toBe(0);
         expect(pacman.speed).toBe(1);
         expect(pacman.dir).toBeNull();
-        // Add more checks for other properties
+        
     });
 
-    // Testing the shouldMove method
+    // test de la méthode shouldMove 
     test('shouldMove returns false when direction is null', () => {
-        const pacman = new Pacman(1, 0); // Adjust the initial properties as needed
+        const pacman = new Pacman(1, 0); 
         pacman.dir = null;
 
         expect(pacman.shouldMove()).toBe(false);
